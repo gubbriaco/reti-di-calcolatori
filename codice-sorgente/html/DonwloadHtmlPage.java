8ff2a0cf306e3a27823b672585d1346f83ce53f0
gubbriaco <101352023+gubbriaco@users.noreply.github.com>
2022-06-23 21:43:48 +0200

    23-06-2022

--- codice-sorgente/html/DonwloadHtmlPage.java
+++ codice-sorgente/html/DonwloadHtmlPage.java
@@ -1,21 +1,37 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package html;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
 
-/**
- *
- * @author giorg
- */
 public class DonwloadHtmlPage {
     
     public static void main(String...strings){
         
         try{
             
+            String URL = "stackoverlow.com";
+            @SuppressWarnings("resource")
+			Socket socket = new Socket(URL, 80);
+            
+            PrintWriter pw = new PrintWriter(socket.getOutputStream());
+            BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            
+            pw.println("GET/questions HTTP/1.1");
+            pw.println("Host: " + URL);
+            pw.println();
+            
+            boolean more = true;
+            String line;
+            while(more) {
+            	line = br.readLine();
+            	if(line==null)
+            		more = false;
+            	else
+            		System.out.println(line);
+            }
             
             
         }catch(IOException e){
