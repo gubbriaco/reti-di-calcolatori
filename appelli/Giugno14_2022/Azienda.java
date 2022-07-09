0eeaa47cbb4a895456db676247ae3da7f278f67f
gubbriaco <101352023+gubbriaco@users.noreply.github.com>
2022-07-10 01:10:06 +0200

    09-07-2022

--- appelli/Giugno14_2022/Azienda.java
+++ appelli/Giugno14_2022/Azienda.java
@@ -7,7 +7,10 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Random;
 
-public class Azienda {
+public class Azienda extends Thread {
+	
+	private final String host_name_server = "job.unical.it";
+	private final int TCP_PORT_INVIO = 3000, TCP_PORT_CANDIDATURA = 5000;
 	
 	private int piva;
 	private String address;
@@ -18,6 +21,32 @@ public class Azienda {
 	}
 	
 	
+	@Override public void run() {
+
+		try {
+			
+			Socket socket_offerta = new Socket(host_name_server, TCP_PORT_INVIO);
+			
+			ObjectOutputStream oos = new ObjectOutputStream(socket_offerta.getOutputStream());
+			Offerta offerta = new Offerta("settore", "ruolo", "tipo", "ral", false, address);
+			oos.writeObject(offerta);
+			oos.flush();
+			
+			ObjectInputStream ois = new ObjectInputStream(socket_offerta.getInputStream());
+			int id = (int)ois.readObject();
+
+			
+			ServerSocket server_candidatura = new ServerSocket(TCP_PORT_CANDIDATURA);
+			Socket socket = server_candidatura.accept();
+			ois = new ObjectInputStream(socket.getInputStream());
+			Candidatura candidatura = (Candidatura)ois.readObject();
+			
+		}catch(IOException |ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
 	public static void main(String...strings) {
 		
 		final int N = 4;
@@ -27,35 +56,7 @@ public class Azienda {
 			Random random = new Random();
 			int piva = random.nextInt();
 			String address = "azienda" + piva + ".unical.it";
-			Azienda azienda = new Azienda(piva, address);
-		
-			final String host_name_server = "job.unical.it";
-			final int TCP_PORT_INVIO = 3000,
-					  TCP_PORT_CANDIDATURA = 5000;
-			
-			try {
-				
-				
-				Socket socket_offerta = new Socket(host_name_server, TCP_PORT_INVIO);
-				
-				ObjectOutputStream oos = new ObjectOutputStream(socket_offerta.getOutputStream());
-				Offerta offerta = new Offerta("settore", "ruolo", "tipo", "ral", false, azienda.address);
-				oos.writeObject(offerta);
-				oos.flush();
-				
-				ObjectInputStream ois = new ObjectInputStream(socket_offerta.getInputStream());
-				int id = (int)ois.readObject();
-	
-				
-				ServerSocket server_candidatura = new ServerSocket(TCP_PORT_CANDIDATURA);
-				Socket socket = server_candidatura.accept();
-				ois = new ObjectInputStream(socket.getInputStream());
-				Candidatura candidatura = (Candidatura)ois.readObject();
-				
-				
-			}catch(IOException |ClassNotFoundException e) {
-				e.printStackTrace();
-			}
+			new Azienda(piva, address).start();
 			
 		}
 		
